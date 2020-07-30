45d7060c7e69255638ad9a101762c3a635ef8eea
Matt Carter <carter.matthew@me.com>
2020-07-30 01:09:05 -0400

    calc app work

---
+++ Main-test.js
@@ -0,0 +1,61 @@
+let current = "0"
+let memory = null
+let operation = null
+
+const AddDigit = (num) => {
+  if (current == 0) {
+    current = num
+  } else {
+    current = current + num
+  }
+  document.getElementById("numberDisplay").value = current
+}
+
+const setOperation = (id) => {
+  memory = current
+  switch (id) {
+    case "addition": operation = id
+    document.getElementById("currentOperation").value = "+"
+    break;
+    case "subtract": operation = id
+    document.getElementById("currentOperation").value = "-"
+    break;
+    case "multiply": operation = id
+    document.getElementById("currentOperation").value = "*"
+    break;
+    case "divide": operation = id
+    document.getElementById("currentOperation").value = "/"
+    break;
+    case "modulus": operation = id
+    document.getElementById("currentOperation").value = "%"
+    break;
+  }
+  document.getElementById("numberDisplay").value = ""
+}
+
+const calculate = () => {
+  let result = null
+  switch (operation) {
+    case "addition": result = parseInt(memory) + parseInt(current)    
+    break;
+    case "subtract": result = parseInt(memory) - parseInt(current)    
+    break;
+    case "multiply": result = parseInt(memory) * parseInt(current)    
+    break;
+    case "divide": result = parseInt(memory) / parseInt(current)    
+    break;
+    case "modulus": result = parseInt(memory) % parseInt(current)    
+    break;
+  }
+  document.getElementById("numberDisplay").value = result
+  document.getElementById("currentOperation").value = "="
+}
+
+const clearOut = () => {
+  document.getElementById("currentOperation").value = ""
+  document.getElementById("numberDisplay").value = ""
+
+  current = null
+  memory = null
+  operation = null
+}
