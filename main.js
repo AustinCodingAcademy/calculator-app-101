3708d2c9cea0ee1bbf048042dc9cbce07ba54c37
omarusmani <omarusmani3@hotmail.com>
2022-02-27 16:31:43 -0600

    calc finish no css

--- main.js
+++ main.js
@@ -19,26 +19,46 @@ const saveSecondNumber = (num) => {
 const add = (numA, numB) => {
   const sum = numA + numB
   return sum
+  console.log(numA, numB)
+  console.log(sum)
 }
 
 // this function takes in two argument/numbers and returns the difference of them
 const subtract = (numA, numB) => {
   const difference = numA - numB
+  console.log(numA, numB)
+  console.log(difference)
   return difference
+
 }
 
 // These variables are already defined but that don't point to functions. It's up to you to build the functions to complete your calculator use:
 
 const multiply = (numA, numB) => {
-  // * to get a product then return it
-  // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
+  const multiple = numA*numB
   console.log(numA, numB)
+  console.log(multiple)
+  return multiple
+  // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
+
 }
 
-const divide = null
-// / to get a quotient,
 
-const modulus = null
+// / to get a quotient,
+const divide = (numA, numB)=> {
+const div =numA/numB
+console.log(numA, numB)
+console.log(div)
+return div
+}
+
+
+const modulus = (numA, numB)=>{
+  const mod =numA%numB
+  console.log(numA, numB)
+  console.log(mod)
+  return mod
+}
 // and % to get a remainder.
 
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
@@ -64,11 +84,11 @@ const equals = () => {
     break;
     case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
     break;
-    case "multiplication": multiply(firstNum, secondNum) 
+    case "multiplication": putResultInElement( multiply(firstNum, secondNum)) 
     break;
-    case "division": console.log(divide(firstNum, secondNum)) 
+    case "division": putResultInElement(divide(firstNum, secondNum)) 
     break;
-    case "modulus": console.log(modulus(firstNum, secondNum)) 
+    case "modulus": putResultInElement(modulus(firstNum, secondNum)) 
     break;
     default: "Choose an operation"
   }
