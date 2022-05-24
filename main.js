16f2fae14eeed4425d6034258dfde7f7880faa3f
Stephen <stephen.lyssy@gmail.com>
2022-05-24 10:54:07 -0500

    formatted page for desktop view.

--- main.js
+++ main.js
@@ -49,14 +49,28 @@ const modulus = (numA, numB) => {
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
 const changeOperation = (chosenOperation) => {
   operation = chosenOperation;
-  // Use your Chrome Inspector Tool > Console Tab to see the "operation" that's logged
+  // Use your Chrome Inspector Tool > Console Tab to see the "operation" that's
+  // logged
+  // if (operation === addition) {
+  //   operation = '+';
+  // } else if (operation === subtraction) {
+  //   operation = '-';
+  // } else if (operation === multiplication) {
+  //   operation = 'x';
+  // } else if (operation === division) {
+  //   operation = '&#247';
+  // } else if (operation === modulus) {
+  //   operation = '%';
+  // }
   console.log(operation);
 };
 
 // In order to show the user their results we have to access the DOM and stick in the value
 const putResultInElement = (operationResults) => {
   // access the DOM by writing "document" then use the method "getElementById" and pass it the id, "result".
-  document.getElementById('result').innerHTML = 'Results: ' + operationResults;
+  document.getElementById(
+    'result'
+  ).innerHTML = `The answer is: ${operationResults}`;
 
   // Remember, each element has built in properties like "innerHTML" which we can change to anything we like.
   // Here we give it a string: "Results: " and add the value of the operation to it.
